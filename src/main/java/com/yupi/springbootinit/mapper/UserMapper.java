d5d560ad86e5009dcd43f6114e180b80fd02f4e9
wei5687 <1094019524@qq.com>
2024-09-22 12:16:56 +0800

    first init

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity generator.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,12 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ *
+ */
+public interface ChartService extends IService<Chart> {
+
+}
